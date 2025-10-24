93084732f5dcabbedd9f7dc6842abc94060d342c
Rijan Gautam <rijangautam07@gmail.com>
2025-10-24 16:43:54 +0545

    initial commit

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -5,7 +5,7 @@ const LoginPage = ()=>{
         // split login page. 
         <div className='w-full h-full flex justify-center items-center '>
             <main className='bg-white border-4 w-[90%] h-[85%]'>
-                <section className="border-black text-9xl">Hii how is the font</section>
+                <section className="border-black text-9xl"></section>
                 <section className='border-black'></section>
             </main>
         </div>
